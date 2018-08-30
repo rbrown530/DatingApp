034db1fdd4b8097529e0e9745633ac44b1cceef5
rbrown530 <5lickerjo3>
2018-08-30 13:14:07 -0500

    end of section 9

--- DatingApp-Spa/src/app/routes.ts
+++ DatingApp-Spa/src/app/routes.ts
@@ -1,18 +1,22 @@
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { AuthGuard } from './_guards/auth.guard';
 import {Routes} from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 
 export const appRoutes: Routes = [
-   { path: '', component: HomeComponent},
+   { path: 'home', component: HomeComponent},
    {    path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
             { path: 'messages', component: MessagesComponent},
-            { path: 'members', component: MemberListComponent},
+            { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+            { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver }},
             { path: 'lists', component: ListsComponent},
         ] },
    { path: '**', redirectTo: 'home', pathMatch: 'full'},
