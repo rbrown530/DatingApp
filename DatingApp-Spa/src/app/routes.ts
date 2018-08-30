7a607ea8316c417ed4e6b906cd01a3ca43ea2031
rbrown530 <5lickerjo3>
2018-08-30 15:33:11 -0500

    end section 10

--- DatingApp-Spa/src/app/routes.ts
+++ DatingApp-Spa/src/app/routes.ts
@@ -1,3 +1,5 @@
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { AuthGuard } from './_guards/auth.guard';
 import {Routes} from '@angular/router';
@@ -7,6 +9,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MessagesComponent } from './messages/messages.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
    { path: 'home', component: HomeComponent},
@@ -17,6 +20,8 @@ export const appRoutes: Routes = [
             { path: 'messages', component: MessagesComponent},
             { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
             { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver }},
+            { path: 'member/edit', component: MemberEditComponent,
+            resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
             { path: 'lists', component: ListsComponent},
         ] },
    { path: '**', redirectTo: 'home', pathMatch: 'full'},
